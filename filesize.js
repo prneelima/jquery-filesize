d706c216f97e1a5a06abfc924ff5e77065af29d4
prneelima <prneelima@gmail.com>
2015-02-16 00:53:27 -0800

    changes made to filesize

--- filesize.js
+++ filesize.js
@@ -1,1 +1,29 @@
-//text content more content
+(function($){
+
+  $.fn.filesize = function (){
+  
+  var filelen = this.attr("size") || 0, filelength;
+  if(filelen === 0 || filelen <1000){
+    filelength = filelen+" B";
+  }
+  else if(filelen >=1000 && filelen <1000000 ){
+    filelength = filelen/1000;
+    filelength = filelength.toFixed(2) + " KB";
+    
+  }else if(filelen >=1000000 && filelen <1000000000 ){
+    filelength = filelen/1000000;
+    filelength = filelength.toFixed(2) + " MB";
+    
+  }
+  else if(filelen >=1000000000 && filelen <1000000000000 ){
+    filelength = filelen/1000000000;
+    filelength = filelength.toFixed(2) + " GB";
+    
+  }else {
+    filelength = filelen/1000000000000;
+    filelength = filelength.toFixed(2) + " TB";
+  }
+
+  this.html(filelength);
+}
+})(jQuery);
